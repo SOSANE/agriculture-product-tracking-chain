edc633589c171beae391704063be50078de4e427
SOS <sosanemh.pro@gmail.com>
2025-05-09 19:50:30 -0400

    update vite-env.d.ts

--- agrichain-frontend-react/agrichain-frontend/src/vite-env.d.ts
+++ agrichain-frontend-react/agrichain-frontend/src/vite-env.d.ts
@@ -2,3 +2,12 @@
 interface Window {
     ethereum?: import('@metamask/providers').MetaMaskInpageProvider;
 }
+
+interface ImportMetaEnv {
+    readonly VITE_CONTRACT_ADDRESS: string;
+    readonly VITE_BLOCKCHAIN_PROVIDER_URL: string;
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv;
+}
