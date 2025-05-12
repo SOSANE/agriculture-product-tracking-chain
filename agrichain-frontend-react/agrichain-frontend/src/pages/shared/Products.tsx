c1da3bde87d2149209fc5decffd36abfd6566bde
SOS <sosanemh.pro@gmail.com>
2025-05-12 10:43:40 -0400

    fix products page

--- agrichain-frontend-react/agrichain-frontend/src/pages/shared/Products.tsx
+++ agrichain-frontend-react/agrichain-frontend/src/pages/shared/Products.tsx
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from "react";
 import {DashboardLayout} from "../../components/layout/DashboardLayout.tsx";
 import {Link} from "react-router-dom";
 import {Product} from "../../types";
+import {Leaf} from "lucide-react";
 
 
 export const Products: React.FC = () => {
@@ -35,27 +36,32 @@ export const Products: React.FC = () => {
                                 View the details for any product registered to the blockchain as a user.
                             </p>
                         </header>
-                        {products.length > 0 && (
-                            products.map((product) => (
-                                <section key={product.id}>
-                                    <div className="card h-full flex flex-col">
-                                        <Link to={`/products/${product.id}`} className="block">
-                                            <div
-                                                className="flex items-start gap-4 p-3 rounded-lg hover:bg-neutral-50 transition-colors">
-                                                <div className="bg-primary bg-opacity-10 p-2 rounded-full">
+
+                        <section>
+                            <div className="card h-full flex flex-col divide-y divide-gray-200">
+                                <div className="flex justify-between items-center mb-6">
+                                    <h2 className="text-xl font-semibold">Recent activity</h2>
+                                    <Leaf className="h-6 w-6 text-primary" />
+                                </div>
+                                {products.length > 0 && (
+                                    products.map((product) => (
+                                        <div key={product.id} className="space-y-2">
+                                            <Link to={`/products/${product.id}`} className="block">
+                                                <div className="flex items-start gap-4 p-3 rounded-lg hover:bg-neutral-50 transition-colors">
+                                                    <div>
+                                                        <p className="font-medium">{product.name}</p>
+                                                        <p className="text-sm text-neutral-600">{product.description}</p>
+                                                        <p className="text-xs text-neutral-500">{product.type}</p>
+                                                        <p className="text-xs text-neutral-500">{product.farmer.name}</p>
+                                                    </div>
                                                 </div>
-                                                <div>
-                                                    <p className="font-medium">{product.name}</p>
-                                                    <p className="text-sm text-neutral-600">{product.description}</p>
-                                                    <p className="text-xs text-neutral-500 mt-1">{product.type}</p>
-                                                    <p className="text-xs text-neutral-500 mt-1">{product.farmer.name}</p>
-                                                </div>
-                                            </div>
-                                        </Link>
-                                    </div>
-                                </section>
-                            ))
-                        )}
+                                            </Link>
+                                        </div>
+                                    ))
+                                )}
+
+                            </div>
+                        </section>
                     </div>
                 </div>
             </div>
